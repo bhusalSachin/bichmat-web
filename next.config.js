adaecdde446718e18baeb138a8b3247e659ca481
bhusalSachin <sachinb1377@gmail.com>
2023-05-31 22:49:30 +0545

    nextconfig env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    LOGIN_ADMIN_URL: "http://localhost:8000/admin/login",
-    ADMIN_ENTER_URL: "http://localhost:8000/admin/enter",
+    LOGIN_ADMIN_URL: "https://bichmat-server.vercel.app/admin/login",
+    ADMIN_ENTER_URL: "https://bichmat-server.vercel.app/admin/enter",
   },
 };
 
